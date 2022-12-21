0868e9620f61548bf79961688423f2b939e944d7
unknown <mr.thatbai@gmail.com>
2022-12-22 00:06:17 +0700

    tao bootstrap

---
+++ user.js
@@ -0,0 +1,14 @@
+class User {
+    id;
+    name;
+    username;
+    password;
+
+    constructor(id, name, username, password) {
+        this.id = id;
+        this.name = name;
+        this.username = username;
+        this.password = password;
+    }
+    set
+}
